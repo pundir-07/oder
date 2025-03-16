1105ea914172430be9144274a6a01b4924f7ca64
Aabhas Dhaka <vaibhavpundir07@gmail.com>
2025-03-16 14:10:20 +0530

    Fixed bestseller 2

--- app/components/BestSellers.tsx
+++ app/components/BestSellers.tsx
@@ -2,19 +2,19 @@
 import React from 'react'
 import HorizontalScroll from './HorizontalScroll'
 import { Item } from '@/app/types'
-import { useState } from 'react'
+// import { useState } from 'react'
 // import HorizontalLoading from './HorizontalLoading'
 import { Tag } from 'lucide-react'
 export default function BestSellers({ items }: { items: Item[] }) {
-    const [imagesLoadedCount, setImagesLoadedCount] = useState(0)
-    const loadingComplete = imagesLoadedCount >= items.length
+    // const [imagesLoadedCount, setImagesLoadedCount] = useState(0)
+    // const loadingComplete = imagesLoadedCount >= items.length
     // console.log(`items number in bestseller - ${items.length}`)
     // console.log(`images loaded in bestseller - ${imagesLoadedCount} `)
     return (
         <div className='relative bg-white py-2'>
             <h2 className="px-4 pb-2 text-lg font-semibold flex items-center gap-1">BestSellers <Tag size={15} color='red' strokeWidth={3} /></h2>
             {/* {!loadingComplete && <HorizontalLoading />} */}
-            {<HorizontalScroll visible={loadingComplete} onLoad={() => setImagesLoadedCount(prev => prev + 1)} items={items} />}
+            {<HorizontalScroll visible={true} onLoad={() => { }} items={items} />}
 
         </div>
     )
