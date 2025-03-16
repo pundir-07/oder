c4ff8e1a07bda63239351c82dbaca853ad596ccb
Aabhas Dhaka <vaibhavpundir07@gmail.com>
2025-03-16 13:43:49 +0530

    Bestsellers loading issue fix

--- app/components/BestSellers.tsx
+++ app/components/BestSellers.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 import HorizontalScroll from './HorizontalScroll'
 import { Item } from '@/app/types'
 import { useState } from 'react'
-import HorizontalLoading from './HorizontalLoading'
+// import HorizontalLoading from './HorizontalLoading'
 import { Tag } from 'lucide-react'
 export default function BestSellers({ items }: { items: Item[] }) {
     const [imagesLoadedCount, setImagesLoadedCount] = useState(0)
@@ -13,7 +13,7 @@ export default function BestSellers({ items }: { items: Item[] }) {
     return (
         <div className='relative bg-white py-2'>
             <h2 className="px-4 pb-2 text-lg font-semibold flex items-center gap-1">BestSellers <Tag size={15} color='red' strokeWidth={3} /></h2>
-            {!loadingComplete && <HorizontalLoading />}
+            {/* {!loadingComplete && <HorizontalLoading />} */}
             {<HorizontalScroll visible={loadingComplete} onLoad={() => setImagesLoadedCount(prev => prev + 1)} items={items} />}
 
         </div>
